44815e06a628ff37fa332565c86de992c2a48654
Cynthia <cturner@ltu.edu>
2019-01-15 18:09:02 -0500

    Finished the lab

--- src/Lab3/ifStatement.java
+++ src/Lab3/ifStatement.java
@@ -7,16 +7,29 @@ public class ifStatement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int userinput;
+		String choice = ("y");
+		
+		while (choice.equalsIgnoreCase("y")) {
+		
 		Scanner scnr = new Scanner(System.in);
 		
-		System.out.println("Give me a number: ");
-		int userinput = scnr.nextInt();
+		
 		
 		//int userinput = 1; userinput <= 100; userinput++) {
 			
+			do {
+				System.out.println("Give me a number: ");
+				userinput = scnr.nextInt();
+				
+				if (userinput < 1 || userinput > 100) {
+					System.out.println("Numbers must be between 1 - 100");
+				}
+			} while(userinput < 1 || userinput > 100);
 			
-					if  ( userinput % 2 != 0) {
-						System.out.print("Odd");
+			
+				if  ( userinput % 2 != 0&& userinput <= 60) {
+						System.out.println("Odd");
 					}
 					else if  ((userinput % 2 == 0)& userinput <= 25) {
 						System.out.println("Even and less than 25");
@@ -28,13 +41,13 @@ public class ifStatement {
 						System.out.println("Even");
 					}
 					else if (userinput % 2 != 0 && userinput > 60) {
-						System.out.print("Odd and over 60");
+						System.out.println("Odd and over 60");
+					
 					}
-					else if  (userinput > 100) {
-						System.out.println("Numbers must be between 1 - 100");
-					}
-					else 
-						System.out.println("That is All");
+					System.out.println("Continue: Press Y or any key to exit");
+					choice = scnr.next();
+				}
+						System.out.println("That is All!");
 			}
 
 	     
