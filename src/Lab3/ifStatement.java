83aed669c71e6303a61787a84fe2a44dfef43941
Cynthia <cturner@ltu.edu>
2019-01-15 18:19:18 -0500

    Added comments

--- src/Lab3/ifStatement.java
+++ src/Lab3/ifStatement.java
@@ -8,17 +8,14 @@ public class ifStatement {
 		// TODO Auto-generated method stub
 		
 		int userinput;
-		String choice = ("y");
+		String choice = ("y");// allows users to pick Y to continue
 		
-		while (choice.equalsIgnoreCase("y")) {
+		while (choice.equalsIgnoreCase("y")) {//This allows input to take any case of y
 		
 		Scanner scnr = new Scanner(System.in);
-		
-		
-		
-		//int userinput = 1; userinput <= 100; userinput++) {
+	
 			
-			do {
+			do {// do - loop to make user pick between 1-100.
 				System.out.println("Give me a number: ");
 				userinput = scnr.nextInt();
 				
@@ -28,29 +25,27 @@ public class ifStatement {
 			} while(userinput < 1 || userinput > 100);
 			
 			
-				if  ( userinput % 2 != 0&& userinput <= 60) {
+				if  ( userinput % 2 != 0 && userinput <= 60) {
 						System.out.println("Odd");
 					}
-					else if  ((userinput % 2 == 0)& userinput <= 25) {
+					else if  ((userinput % 2 == 0) & userinput <= 25) {
 						System.out.println("Even and less than 25");
 					}
-					else if ((userinput % 2 == 0)& userinput > 25 && userinput <= 60) {
+					else if ((userinput % 2 == 0) & userinput > 25 && userinput <= 60) {
 						System.out.println("Even");
 					}
-					else if ((userinput % 2 == 0)& userinput > 60) {
+					else if ((userinput % 2 == 0) & userinput > 60) {
 						System.out.println("Even");
 					}
 					else if (userinput % 2 != 0 && userinput > 60) {
 						System.out.println("Odd and over 60");
 					
 					}
-					System.out.println("Continue: Press Y or any key to exit");
+					System.out.println("Continue: Press Y or any key to exit");//Allows user to exit program
 					choice = scnr.next();
 				}
-						System.out.println("That is All!");
+						System.out.println("STEP AWAY FROM THE PRO!");
 			}
-
-	     
 	} 
 
 		
